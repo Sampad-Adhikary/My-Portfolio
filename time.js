057b35ef56cfec7436a7cba6374760d37ff57b94
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-03-05 01:53:26 +0530

    march 5 midnight

--- time.js
+++ time.js
@@ -17,6 +17,8 @@ exports.currTime = function(){
     var ampm = hours >= 12 ? ' PM' : ' AM';
     hours = hours % 12;
     hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    hours = hours < 10 ? '0'+hours : hours;
     var time = hours + ":" + minutes + ampm;
     return time;
 }
